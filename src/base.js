dc7d58806adaa5acebbaa143bfc4565fd0cbedd8
Katy <ihatetoast@gmail.com>
2017-04-30 21:07:19 -0500

    added firebase rebase and const base

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyAJrGPkENDVW31yVX6Nkpl6e7CbHk7PzaY",
+    authDomain: "unscrambled-47ddd.firebaseapp.com",
+    databaseURL: "https://unscrambled-47ddd.firebaseio.com"
+});
+
+export default base;
